50fb37073aeee2732b64349bf0b449693524b651
penolakushari <45951611+penolakushari@users.noreply.github.com>
2022-01-17 21:36:50 +0300

    Updated the camera attacher, so it will parent camera to the player as long as it works, so it wouldn't lag behind him. Makes vehicle entrance parts kind of weird though

--- cameraattacher/lua/autorun/advcamera_attacher.lua
+++ cameraattacher/lua/autorun/advcamera_attacher.lua
@@ -6,8 +6,13 @@ local OnlyProperties = false
 concommand.Add("penak_camera", function(ply, cmd, args)
 	if CLIENT then return end
 
-	if ply:GetEyeTrace().Entity:GetClass() == "hl_camera" then
+	if ply:GetEyeTrace().Entity:GetClass() == "hl_camera" then -- It only works with advanced cameras, and their entity class is hl_camera
+
 		ChosenEntity = ply:GetEyeTrace().Entity
+		ChosenEntity:SetPos(Entity(1):EyePos())
+		ChosenEntity:SetAngles(Entity(1):EyeAngles())
+		ChosenEntity:SetParent(Entity(1), 0)
+
 		OnlyProperties = false
 		ply:SelectWeapon("gmod_camera")
 	end
@@ -29,12 +34,18 @@ hook.Add("Think", "AdvCameraAttach", function()
 	if IsValid(ChosenEntity) then
 		if Entity(1):GetActiveWeapon():GetClass() == "gmod_camera" then
 			if not OnlyProperties then
-				ChosenEntity:SetPos(Entity(1):EyePos())
+				ChosenEntity:SetPos(Entity(1):WorldToLocal(Entity(1):EyePos()))
 				ChosenEntity:SetAngles(Entity(1):EyeAngles())
 			end
+
 			ChosenEntity:SetRoll(Entity(1):GetActiveWeapon():GetRoll())
 			ChosenEntity:SetFOV(Entity(1):GetActiveWeapon():GetZoom())
 		else
+			ChosenEntity:SetParent(nil)
+			if not OnlyProperties then
+				ChosenEntity:SetPos(Entity(1):EyePos()) -- Positioning camera to player's view after unparenting it, just in case.
+				ChosenEntity:SetAngles(Entity(1):EyeAngles())
+			end
 			ChosenEntity = nil
 		end
 	end
