daed091f3121399f0055c776bd9291daab033fc4
penolakushari <45951611+penolakushari@users.noreply.github.com>
2022-07-03 15:24:27 +0300

    Made turn to dynamic prop tool able to turn entities into effect props

--- light origin tool/lua/weapons/gmod_tool/stools/turntodynamic.lua
+++ light origin tool/lua/weapons/gmod_tool/stools/turntodynamic.lua
@@ -3,8 +3,30 @@ TOOL.Name			= "Turn Into Dynamic Prop"
 TOOL.Command		= nil
 TOOL.ConfigName		= nil
 
-
 function TOOL:LeftClick(tr)
+
+	if !IsValid(tr.Entity) then return false end
+	if tr.Entity:GetClass() == "prop_effect" then return false end
+
+	if CLIENT then return true end
+	local ent = tr.Entity
+
+	local newent = ents.Create("prop_effect")
+	newent:SetModel(ent:GetModel())
+	newent:SetPos(ent:GetPos())
+	newent:SetAngles(ent:GetAngles())
+	newent:Spawn()
+	ent:Remove()
+
+	undo.Create("Entity to prop_effect")
+		undo.AddEntity(newent)
+		undo.SetPlayer(self:GetOwner())
+	undo.Finish("Entity to Effect Prop (" .. newent:GetModel() .. ")")
+
+	return true		
+end
+
+function TOOL:RightClick(tr)
 
 	if !IsValid(tr.Entity) then return false end
 	
@@ -25,7 +47,7 @@ function TOOL:LeftClick(tr)
 	newent:Spawn()
 	ent:Remove()
 	
-	undo.Create("entitytodynamic")
+	undo.Create("Entity to prop_dynamic")
 		undo.AddEntity(newent)
 		undo.SetPlayer(self:GetOwner())
 	undo.Finish("Entity to Dynamic Prop (" .. newent:GetModel() .. ")")
@@ -36,7 +58,7 @@ end
 if CLIENT then
 
 language.Add("tool.turntodynamic.name","Turn Into Dynamic Prop")
-language.Add("tool.turntodynamic.desc","Turn entities into prop_dynamic")
-language.Add("tool.turntodynamic.0","Left Click on an entity to turn it into prop_dynamic.")
+language.Add("tool.turntodynamic.desc","Turn entities into prop_effect/prop_dynamic")
+language.Add("tool.turntodynamic.0","Left Click on an entity to turn it into effect prop. Right click to turn it into prop_dynamic")
 
 end
