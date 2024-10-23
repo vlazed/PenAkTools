46539d4206c33ccb5d4e91507c241b5244482871
vlazed <35244845+vlazed@users.noreply.github.com>
2024-10-22 20:39:21 -0500

    Add presets, optimize, and add typing
    - Add presets and options from the demonstration video
    - Add LuaLS typing
    - Use the returned ConVar in CreateClientConVar to prevent lookups using GetConVar. Deprecates GetConVar
    - Use chat.AddText and ConCommand function instead of indexing the player methods ChatPrint and ConCommand. Prevents calls to localplayer and also reduces lookups through player table

--- macroreplacement/lua/autorun/client/macroreplacerthing.lua
+++ macroreplacement/lua/autorun/client/macroreplacerthing.lua
@@ -1,49 +1,149 @@
+---@class MacroData ConVar table that contains the Macro Replacer ConVars as keys and their values
+---@field macrothing_wait1 string
+---@field macrothing_wait2 string
+---@field macrothing_execute1 string
+---@field macrothing_execute2 string
+
 if not game.SinglePlayer() then return end
 
-local Working = false
-local Stop = false
-
-local function GrabConvar( name )
-	return GetConVar( name ):GetFloat()
+-- Prevent lookup cost by localizing functions 
+local addText = chat.AddText
+local conCommand
+do 
+	---@type Player
+	local Player = FindMetaTable("Player")
+	conCommand = Player.ConCommand
 end
 
-local function MacroTick( loop )
-	if loop < 1 or Stop then 
-		LocalPlayer():ChatPrint("MacroThing finished!")
-		Stop = false
-		Working = false
-		return
+-- CreateClientConVar returns the ConVar itself. Store them here so we can reference them for later 
+local loops = CreateClientConVar( "macrothing_loops", "30", true, false, nil, 1, nil )
+local startWait = CreateClientConVar( "macrothing_startwait", "1", true, false, nil, 0, nil )
+local execute1 = CreateClientConVar( "macrothing_execute1", "", true, false, nil, nil, nil )
+local wait1 = CreateClientConVar( "macrothing_wait1", "1", true, false, nil, 0, nil )
+local execute2 = CreateClientConVar( "macrothing_execute2", "", true, false, nil, nil, nil )
+local wait2 = CreateClientConVar( "macrothing_wait2", "0.5", true, false, nil, 0, nil )
+
+-- Scope the below functions as they don't impact the UI building functions
+do
+	local Working = false
+	local Stop = false
+
+	---@param loop integer
+	local function MacroTick( loop )
+		if loop < 1 or Stop then 
+			addText("MacroThing finished!")
+			Stop = false
+			Working = false
+			return
+		end
+		---@type Player
+		local localPlayer = LocalPlayer()
+
+		conCommand(localPlayer, execute1:GetString() )
+		timer.Simple( wait1:GetFloat(), function()
+			conCommand(localPlayer, execute2:GetString() )
+
+			timer.Simple( wait2:GetFloat(), function()
+				MacroTick(loop - 1)
+			end )
+		end	)
 	end
 
-	LocalPlayer():ConCommand( GetConVar( "macrothing_execute1" ):GetString() )
-	timer.Simple( GrabConvar( "macrothing_wait1" ), function()
-		LocalPlayer():ConCommand( GetConVar( "macrothing_execute2" ):GetString() )
-
-		timer.Simple( GrabConvar( "macrothing_wait2" ), function()
-			MacroTick(loop - 1)
-		end )
-	end	)
+	concommand.Add( "macrothing_start", function()
+		if not Working then
+			Working = true
+			timer.Simple( startWait:GetFloat(), function()
+				MacroTick( loops:GetInt() )
+			end )
+		else
+			Stop = true
+		end
+	end )
 end
 
-CreateClientConVar( "macrothing_loops", 30, true, false, nil, 1, nil )
-CreateClientConVar( "macrothing_startwait", 1, true, false, nil, 0, nil )
-CreateClientConVar( "macrothing_execute1", "", true, false, nil, nil, nil )
-CreateClientConVar( "macrothing_wait1", 1, true, false, nil, 0, nil )
-CreateClientConVar( "macrothing_execute2", "", true, false, nil, nil, nil )
-CreateClientConVar( "macrothing_wait2", 0.5, true, false, nil, 0, nil )
+---@param wait1 any
+---@param wait2 any
+---@param execute1 any
+---@param execute2 any
+---@return MacroData
+local function macroOption(wait1, wait2, execute1, execute2)
+	return {
+		macrothing_wait1 = tostring(wait1),
+		macrothing_wait2 = tostring(wait2),
+		macrothing_execute1 = tostring(execute1),
+		macrothing_execute2 = tostring(execute2)
+	}
+end
 
-concommand.Add( "macrothing_start", function()
-	if not Working then
-		Working = true
-		timer.Simple( GrabConvar( "macrothing_startwait" ), function()
-			MacroTick( GetConVar( "macrothing_loops" ):GetInt() )
-		end )
-	else
-		Stop = true
-	end
-end )
+-- Add defaults seen from https://www.youtube.com/watch?v=YQUmgGY1Pds
+---@param controlPresets ControlPresets
+local function addDefaults(controlPresets)
+	controlPresets:AddOption("SMH jpeg", macroOption(
+		0.0010, 
+		0.0010, 
+		"jpeg", 
+		"smh_next"
+	))
 
-function MacroBuild( Panel )
+	controlPresets:AddOption("SMH tga", macroOption(
+		0.0010, 
+		0.0010, 
+		"screenshot", 
+		"smh_next"
+	))
+
+	controlPresets:AddOption("SMH jpeg with material manipulation", macroOption(
+		0.0010, 
+		0.0010, 
+		"jpeg", 
+		"smh_next;penak_scroll_next"
+	))
+
+	controlPresets:AddOption("SMH jpeg with soft lamps", macroOption(
+		0.51, 
+		0.51, 
+		"jpeg", 
+		"smh_next"
+	))
+
+	controlPresets:AddOption("SMH jpeg with fake depth", macroOption(
+		0.0010, 
+		0.0010, 
+		"fakedepth_render;jpeg", 
+		"fakedepth_backtogame;smh_next"
+	))
+
+	controlPresets:AddOption("SMH tga with material manipulation", macroOption(
+		0.0010, 
+		0.0010, 
+		"screenshot", 
+		"smh_next;penak_scroll_next"
+	))
+
+	controlPresets:AddOption("SMH tga with soft lamps", macroOption(
+		0.51, 
+		0.51, 
+		"screenshot", 
+		"smh_next"
+	))
+
+	controlPresets:AddOption("SMH tga with fake depth", macroOption(
+		0.0010, 
+		0.0010, 
+		"fakedepth_render;screenshot", 
+		"fakedepth_backtogame;smh_next"
+	))
+end
+
+---@param Panel DForm
+local function MacroBuild( Panel )
+	---@param name string
+	---@param default number
+	---@param min number
+	---@param max number
+	---@param convar string
+	---@param decimals number
+	---@return DNumSlider
 	local function MakeSlider( name, default, min, max, convar, decimals )
 		local newslider = vgui.Create( "DNumSlider", Panel )
 		newslider:SetDefaultValue( default )
@@ -58,29 +158,55 @@ function MacroBuild( Panel )
 		return newslider
 	end
 
-	local function MakeTextBox( convar )
+	---@param convar string
+	---@param convarObj ConVar
+	---@return DTextEntry | Panel
+	local function MakeTextBox( convar, convarObj )
+		---@type DTextEntry | Panel
 		local newtextentry = vgui.Create( "DTextEntry", Panel )
 		newtextentry:SetConVar( convar )
-		newtextentry:SetText( GetConVar( convar ):GetString() )
+		newtextentry:SetText( convarObj and convarObj:GetString() or GetConVar( convar ):GetString() )
 		newtextentry:SetSize( 100, 20 )
 		newtextentry:Dock( TOP )
 		newtextentry:DockMargin( 0, 5, 0, 5 )
 		return newtextentry
 	end
 
-	MakeSlider( "Number of repeats:", GetConVar( "macrothing_loops" ):GetInt(), 1, 300, "macrothing_loops", 0 )
-	MakeSlider( "Wait before starting:", GrabConvar( "macrothing_startwait" ), 0, 10, "macrothing_startwait", 3 )
-	MakeTextBox( "macrothing_execute1" )
-	MakeSlider( "Wait:", GrabConvar( "macrothing_wait1" ), 0, 10, "macrothing_wait1", 4 )
-	MakeTextBox( "macrothing_execute2" )
-	MakeSlider( "Wait:", GrabConvar( "macrothing_wait2" ), 0, 10, "macrothing_wait2", 4 )
+	local function MakeControlPreset()		
+		local controlPresets = vgui.Create("ControlPresets", Panel)
+		controlPresets:Dock( TOP )
+		controlPresets:DockMargin( 0, 5, 0, 5 )
+		controlPresets:AddConVar("macrothing_wait1")
+		controlPresets:AddConVar("macrothing_wait2")
+		controlPresets:AddConVar("macrothing_execute1")
+		controlPresets:AddConVar("macrothing_execute2")
+		controlPresets:SetPreset("Macro Replacer")
+		presets.Add("Macro Replacer", "Last saved preset", {
+			macrothing_wait1 = wait1:GetString(),
+			macrothing_wait2 = wait2:GetString(),
+			macrothing_execute1 = execute1:GetString(),
+			macrothing_execute2 = execute2:GetString(),
+		})
+		return controlPresets
+	end
+
+	local controlPresets = MakeControlPreset()
+
+	MakeSlider( "Number of repeats:", loops:GetInt(), 1, 300, "macrothing_loops", 0 )
+	MakeSlider( "Wait before starting:", startWait:GetFloat(), 0, 10, "macrothing_startwait", 3 )
+	MakeTextBox( "macrothing_execute1", execute1 )
+	MakeSlider( "Wait:", wait1:GetFloat(), 0, 10, "macrothing_wait1", 4 )
+	MakeTextBox( "macrothing_execute2", execute2 )
+	MakeSlider( "Wait:", wait2:GetFloat(), 0, 10, "macrothing_wait2", 4 )
+
+	addDefaults(controlPresets)
 
 	local startbutton = vgui.Create( "DButton", Panel )
 	startbutton:SetSize( 100, 20 )
 	startbutton:SetText( "Start" )
-
+	
 	startbutton.DoClick = function()
-		LocalPlayer():ConCommand( "macrothing_start" )
+		RunConsoleCommand( "macrothing_start" )
 	end
 	startbutton:Dock( TOP )
 	startbutton:DockMargin( 0, 5, 0, 5 )
